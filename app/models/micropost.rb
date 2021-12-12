a9a05ee5667b883e342731f47daa6e116c774be7
noma <noma.developper@gmail.com>
2021-12-12 08:10:58 +0000

    dependent destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,7 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :favorites
-  has_many :fans,through: :favorites, source: :user
+  has_many :favorites,dependent: :destroy
+  has_many :fans,through: :favorites, source: :user,dependent: :destroy
   
 end
